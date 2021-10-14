9a53ba8ae7e4d736370f2e64bc29504310c55fdf
aljoharah21 <aljoharah.ali.ruh@tuwaiq.edu.sa>
2021-10-14 09:02:30 +0300

    my lap

---
+++ pok.js
@@ -0,0 +1,26 @@
+class Pokemon{
+constructor(name, type, color) {
+this.name = name;
+this.type = type;
+this.color = color
+
+
+this.showmore =function() {
+   return this.name +this.type+this.color  
+}
+}
+}
+
+const Pokemon1 = new Pokemon ("picacho","electric","yallow")
+const Pokemon1 = new Pokemon ("egyee","normal","brow")
+const Pokemon1 = new Pokemon ("squitle","water","pink")
+
+console.log(Pokemon1).showeinfs()
+console.log(Pokemon2).showeinfs()
+console.log(Pokemon3).showeinfs()
+
+
+
+
+
+
